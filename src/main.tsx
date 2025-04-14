ef97bd988f4f03b4ada754f6782cd8eb68ae6a21
kdonrmadusanka <ravmkdon@gmail.com>
2025-04-14 19:16:06 +0530

    Wrap the App with BrowserRouter as it is needed to navigate through pages.

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { ThemeProvider } from '@mui/material'
 import theme from './styles/theme.ts'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider theme={ theme }>
-      <App />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={ theme }>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
